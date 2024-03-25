9286ce464d1c0fed9ea567d2aab1da50b2fd71a4
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2024-03-25 18:40:24 +0700

    testing

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/deploy', (req, res) => {
 
 // Define a route handler for the root path
 app.get('/', (req, res) => {
-  res.send('Hello, polygon!');
+  res.send('Hello, pantek!');
 });
 
 // Start the server
