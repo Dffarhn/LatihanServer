797b9d11de09aca2f535a9993a59f5172a342361
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2024-03-25 18:29:47 +0700

    push

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/deploy', (req, res) => {
 
 // Define a route handler for the root path
 app.get('/', (req, res) => {
-  res.send('Hello, bis!');
+  res.send('Hello, najwa!');
 });
 
 // Start the server
