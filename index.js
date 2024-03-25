2323c123e2d77cb1c397a998e0f234765fbff5df
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2024-03-25 18:32:32 +0700

    polygon

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/deploy', (req, res) => {
 
 // Define a route handler for the root path
 app.get('/', (req, res) => {
-  res.send('Hello, najwa!');
+  res.send('Hello, polygon!');
 });
 
 // Start the server
