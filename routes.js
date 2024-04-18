ab53d732a91b90eefbaa4ec93d13f3567260ff21
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2024-04-18 14:58:14 +0800

    ci

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const route = Router();
 
 const sapa = (req, res) => {
-  res.send("halo guys disapa");
+  res.send("halo guys disapa dari device 2 perubahan");
 };
 
 const salam = (req, res) => {};
